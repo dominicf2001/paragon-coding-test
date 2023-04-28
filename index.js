c4424e0212cbf2621b4054d2782703cc8e5a441a
Dominic Ferrando <dominicf2001@gmail.com>
2023-04-28 12:19:48 -0400

    Add input and output elements to html and make initial query selects on them in the js file

---
+++ index.js
@@ -0,0 +1,3 @@
+const dateInput = document.querySelector("#dateInput");
+const outputHolidayDate = document.querySelector("#outputHolidayDate");
+const outputHolidayName = document.querySelector("#outputHolidayName");
